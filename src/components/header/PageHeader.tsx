3bc7583cb39311c0f13792e072b0a65472f94771
阮传宙 <chuanzhou1012@163.com>
2021-10-31 02:51:52 +0800

    fix: 调整头部根据路由的meta来控制是否显示

--- src/components/header/PageHeader.tsx
+++ src/components/header/PageHeader.tsx
@@ -2,10 +2,13 @@ import { defineComponent } from 'vue'
 import HeaderLeft from '@/components/header/HeaderLeft'
 import HeaderRight from '@/components/header/HeaderRight'
 import styles from './pageHeader.module.scss'
+import { useRoute } from 'vue-router'
 export default defineComponent({
   name: 'PageHeader',
   setup() {
     return () => {
+      const route = useRoute()
+      if (route.meta?.disableHeader) return undefined
       return (
         <el-row gutter={20} class={styles.header}>
           <el-col span={20}>
