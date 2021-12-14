8e3d6575d33e03230035cd1369de3803e930b0eb
阮传宙 <chuanzhou1012@163.com>
2021-12-14 16:04:41 +0800

    feat: 添加本地存储工具类型

---
+++ src/Event.d.ts
@@ -0,0 +1,3 @@
+interface Event {
+  newValue: any
+}
---
+++ src/utils/storage.ts
@@ -0,0 +1,83 @@
+interface KeyType {
+  value: any
+  expires?: number
+}
+export default class Storage {
+  // 默认的作用域
+  namespace = '__default__'
+
+  constructor(namespace?: string) {
+    if (namespace) this.namespace = namespace
+  }
+
+  /**
+   * 设置key的值
+   * @param key 键
+   * @param val 值
+   * @param expires 有效期，时间单位秒，
+   */
+  setItem(key: string, val: any, expires?: number) {
+    const setEvent = new Event(key)
+    setEvent.newValue = val
+    const valueObj: KeyType = {
+      value: val,
+      expires: this.calcExpiresTime(expires)
+    }
+    const storage = this.getStorage()
+    storage[key] = valueObj
+    dispatchEvent(setEvent)
+
+    this.updateStorage(storage)
+  }
+
+  /**
+   * 当setItem的key值发生变化时触发
+   * @param event
+   * @param callback
+   */
+  on(event: string, callback: EventListenerOrEventListenerObject) {
+    addEventListener(event, callback)
+  }
+
+  /**
+   * 计算有效时间
+   * @param expires 单位秒
+   */
+  calcExpiresTime(expires?: number) {
+    if (!expires) return undefined
+    return Date.now() + expires * 1000
+  }
+
+  // 获取值
+  getItem(key: string) {
+    const storage = this.getStorage()
+    if (!storage[key]) return null
+    if (storage[key].expires < Date.now()) {
+      this.removeItem(key)
+      return null
+    }
+    return storage[key].value
+  }
+
+  // 删除值
+  removeItem(key: string) {
+    const storage = this.getStorage()
+    delete storage[key]
+    this.updateStorage(storage)
+  }
+
+  // 清除作用域下的所有值，非整个localStorage
+  clearAll() {
+    localStorage.removeItem(this.namespace)
+  }
+
+  // 每次操作之后需要把值更新到localStorage
+  updateStorage(storage: any) {
+    localStorage.setItem(this.namespace, JSON.stringify(storage))
+  }
+
+  // 获取改作用域全部的数据
+  getStorage() {
+    return JSON.parse(localStorage.getItem(this.namespace) ?? '{}')
+  }
+}
