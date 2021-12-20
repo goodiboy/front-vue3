fde4b2187c2e079f0dd17b46b718366791b2fde5
阮传宙 <chuanzhou1012@163.com>
2021-12-21 02:11:58 +0800

    fix: 调整文件名称

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "@/assets/config-style/variable.scss";`
+        additionalData: `@import "@/assets/theme/variable.scss";`
       }
     }
   },
