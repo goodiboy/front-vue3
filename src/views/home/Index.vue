8d5c04218c800bd370ed7c04037d859ecf79f866
阮传宙 <chuanzhou1012@163.com>
2022-01-24 19:25:07 +0800

    feat: 完成菜单布局

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -1,16 +1,57 @@
 <script setup lang="ts">
+  import { Setting, Fold } from '@element-plus/icons'
+
   console.log('index')
+  const isCollapse = false
 </script>
 <template>
   <div class="home">
     <div class="basic-layout">
-      <div class="nav-side"></div>
+      <div :class="['nav-side', isCollapse ? 'fold' : 'unfold']">
+        <!-- 系统logo -->
+        <div class="logo">
+          <img src="@/assets/img/common/logo.png" alt="" />
+          <span>Manager</span>
+        </div>
+        <!-- 导航菜单 -->
+        <el-menu
+          default-active="2"
+          background-color="#001529"
+          text-color="#fff"
+          router
+          :collapse="isCollapse"
+          class="nav-menu"
+        >
+          <!--el-submenu父菜单  -->
+          <el-sub-menu index="1">
+            <template #title>
+              <el-icon><setting /></el-icon>
+              <span>系统管理</span>
+            </template>
+            <!--el-menu-item子菜单  -->
+            <el-menu-item index="1-1">用户管理 </el-menu-item>
+            <el-menu-item index="1-2"> 菜单管理 </el-menu-item>
+          </el-sub-menu>
+          <el-sub-menu index="2">
+            <template #title>
+              <el-icon><setting /></el-icon>
+              <span>审批管理</span>
+            </template>
+            <!--el-menu-item子菜单  -->
+            <el-menu-item index="2-1">休假申请</el-menu-item>
+            <el-menu-item index="2-2">待我审批</el-menu-item>
+          </el-sub-menu>
+        </el-menu>
+      </div>
       <div class="content-right">
         <div class="nav-top">
-          <div class="bread">面包屑</div>
-          <div class="user-info">
-            <user-info />
+          <div class="nav-left">
+            <el-icon class="menu-fold" @click="toggle"><fold /></el-icon>
+
+            <div class="bread">面包屑</div>
           </div>
+
+          <user-info />
         </div>
         <div class="wrapper">
           <div class="main-page">
@@ -35,11 +76,45 @@
       color: #fff;
       background-color: #001529;
       transition: width 0.5s;
+
+      .logo {
+        display: flex;
+        align-items: center;
+        height: 50px;
+        font-size: 18px;
+
+        img {
+          width: 32px;
+          height: 32px;
+          margin: 0 16px;
+        }
+      }
+
+      .nav-menu {
+        height: calc(100vh - 50px);
+        border-right: none;
+      }
+      // 合并左侧菜单
+      &.fold {
+        width: 64px;
+      }
+      // 展开左侧菜单
+      &.unfold {
+        width: 200px;
+      }
     }
 
     /** 右侧页面内容样式 **/
     .content-right {
       margin-left: 200px;
+      // 合并左侧菜单
+      &.fold {
+        margin-left: 64px;
+      }
+      // 展开左侧菜单
+      &.unfold {
+        margin-left: 200px;
+      }
 
       /** 右侧导航内容样式 **/
       .nav-top {
@@ -49,6 +124,16 @@
         padding: 0 20px;
         line-height: 50px;
         border-bottom: 1px solid #ddd;
+
+        .nav-left {
+          display: flex;
+          align-items: center;
+
+          .menu-fold {
+            margin-right: 15px;
+            font-size: 18px;
+          }
+        }
       }
 
       /**  内容样式  **/
