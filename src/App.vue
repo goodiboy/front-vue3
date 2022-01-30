41762c854b71eac24adcb45c778cf3616658ce9b
阮传宙 <chuanzhou1012@163.com>
2022-01-30 20:54:36 +0800

    fix: 调整全局配置的枚举

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,10 @@
   // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
   import { ElConfigProvider } from 'element-plus'
   import run from './utils/run'
-  const size = 'small' // element-plus全局设置默认大小为small
   run()
 </script>
 <template>
-  <el-config-provider :size="size">
+  <el-config-provider size="small">
     <router-view></router-view>
   </el-config-provider>
 </template>
